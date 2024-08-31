f42f6c3bd8e405ec87a7f95cbe441caa8e3cb380
Your Name <you@example.com>
2024-08-31 16:07:27 +0300

    Implemented getDataPoint and getRatio functions with tests

--- client_test.py
+++ client_test.py
@@ -2,14 +2,18 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
+ def test_getDataPoint_calculatePrice(self):
     quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        {'top_ask': {'price': 121.2, 'size': 36}, 'top_bid': {'price': 120.48, 'size': 109}, 'stock': 'ABC'},
+        {'top_ask': {'price': 121.68, 'size': 4}, 'top_bid': {'price': 117.87, 'size': 81}, 'stock': 'DEF'}
     ]
+    for quote in quotes:
+        stock, bid_price, ask_price, price = getDataPoint(quote)
+        self.assertEqual(price, (bid_price + ask_price) / 2)
+
     """ ------------ Add the assertion below ------------ """
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
@@ -17,7 +21,7 @@ class ClientTest(unittest.TestCase):
     """ ------------ Add the assertion below ------------ """
 
 
-  """ ------------ Add more unit tests ------------ """
+""" ------------ Add more unit tests ------------ """
 
 
 
